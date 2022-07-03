ed49e6f19738b03d2f4649bfa195d6b08699ad3e
Caio Fuccio <caio.fuccio@gmail.com>
2022-07-03 16:11:48 -0300

    Adapting code to work with env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { TasksModule } from './tasks/tasks.module';
 
+console.log(process.env);
 @Module({
   imports: [
-    TasksModule,
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'task-management',
-      autoLoadEntities: true,
+      type: process.env.DB_CONNECTION_TYPE as 'postgres',
+      host: process.env.DB_CONNECTION_HOST,
+      port: +process.env.DB_CONNECTION_PORT,
+      username: process.env.DB_CONNECTION_USERNAME,
+      password: process.env.DB_CONNECTION_PASSWORD,
+      database: process.env.DB_CONNECTION_DATABASE,
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
       synchronize: true,
     }),
+    TasksModule,
   ],
   controllers: [],
   providers: [],
